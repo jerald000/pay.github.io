0aeb05d319b99ccf672c0f7a42869df464636a49
UnKnown <85747019+jerald000@users.noreply.github.com>
2023-02-08 22:39:16 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect('https://demo.phpmyadmin.net/master-config/index.php?route=/','root','','ecpay');
+$con=mysqli_connect('https://demo.phpmyadmin.net/master-config/','root','','ecpay');
 ?>
